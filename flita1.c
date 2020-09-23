2cf09b2d3992296a51059e8d43cb8bea6ad1c049
Interdott <56249049+Interdott@users.noreply.github.com>
2020-09-24 00:37:46 +0300

    Update flita1.c

--- flita1.c
+++ flita1.c
@@ -12,30 +12,14 @@ void print_list(node_t * head) {
 
     while (current != NULL) {
         printf("Value: %d\n", current->val);
-        printf("Address: %p\n", (void *)current->next);
         current = current->next;
     }
-}
-
-void push(node_t * head, int val) {
-
-    node_t * current = head;
-
-    while (current->next != NULL) {
-        current = current->next;
-    }
-
-    /* now we can add a new variable */
-    current->next = malloc(sizeof(node_t));
-    current->next->val = val;
-    current->next->next = NULL;
 }
 
 void push_start(node_t ** head, int val) {
 
     node_t * new_node;
     new_node = malloc(sizeof(node_t));
-
     new_node->val = val;
     new_node->next = *head;
     *head = new_node;
@@ -58,58 +42,7 @@ int pop(node_t ** head) {
     return retval;
 }
 
-int remove_last(node_t * head) {
-
-    int retval = 0;
-
-    /* if there is only one item in the list, remove it */
-    if (head->next == NULL) {
-        retval = head->val;
-        free(head);
-        return retval;
-    }
-
-    /* get to the last node in the list */
-    node_t * current = head;
-    while (current->next->next != NULL) {
-        current = current->next;
-    }
-
-    /* now current points to the last item of the list, so let's remove current->next */
-    retval = current->next->val;
-    free(current->next);
-    current->next = NULL;
-    return retval;
-}
-
-int remove_by_index(node_t ** head, int n) {
-
-    int i = 0;
-    int retval = -1;
-
-    node_t * current = *head;
-    node_t * temp_node = NULL;
-
-    if (n == 0) {
-        return pop(head);
-    }
-
-    for (int i = 0; i < n-1; i++) {
-        if (current->next == NULL) {
-            return -1;
-        }
-        current = current->next;
-    }
-
-    temp_node = current->next;
-    retval = temp_node->val;
-    current->next = temp_node->next;
-    free(temp_node);
-
-    return retval;
-}
-
-int main() {
+int main(void) {
 
     node_t * head = NULL;
     head = malloc(sizeof(node_t));
@@ -126,6 +59,12 @@ int main() {
     head->next->next->next = malloc(sizeof(node_t));
     head->next->next->next->val = 4;
     head->next->next->next->next = NULL;
-
+    
+    printf("\nPrint list\n\n");
+    print_list(head);
+    printf("\nDelete last element\n\n");
+    pop(&head);
+    print_list(head);    
+    
     return 0;
 }
